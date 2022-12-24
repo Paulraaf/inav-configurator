9530120d621b74de6639238327846a48a9a59f49
Darren Lines <darren@darrenlines.uk>
2022-12-24 18:47:37 +0000

    ESP32 Radar: Use cardinal markers, plus pan enhancements
    Created new branch based on MrD_Use-cardinal-indicator-for-ESP32-Radar. Due to problems updating from master.

--- tabs/osd.js
+++ tabs/osd.js
@@ -113,6 +113,7 @@ SYM.FLIGHT_MINS_REMAINING = 0xDA;
 SYM.FLIGHT_DIST_REMAINING = 0x167;
 SYM.GROUND_COURSE = 0xDC;
 SYM.CROSS_TRACK_ERROR = 0xFC;
+SYM.PAN_SERVO_IS_OFFSET_L = 0x1C7;
 
 SYM.AH_AIRCRAFT0 = 0x1A2;
 SYM.AH_AIRCRAFT1 = 0x1A3;
@@ -955,6 +956,12 @@ OSD.constants = {
                                 return FONT.symbol(SYM.GLIDE_RANGE) + FONT.embed_dot(' 21') + FONT.symbol(SYM.KM);
                         }
                     }
+                },
+                {
+                    name: 'PAN_SERVO_CENTRED',
+                    id: 142,
+                    min_version: '6.0.0',
+                    preview: FONT.symbol(SYM.PAN_SERVO_IS_OFFSET_L) + '120' + FONT.symbol(SYM.DEGREES)
                 },
                 {
                     name: 'MISSION INFO',
@@ -2523,7 +2530,7 @@ OSD.GUI.updateFields = function() {
             $('<div class="helpicon cf_tip"></div>')
                 .css('margin-top', '1px')
                 .attr('title', groupHelp)
-                .appendTo(groupTitleContainer)
+                .appendTo(groupTitleContainer.parent())
                 .jBox('Tooltip', {
                     delayOpen: 100,
                     delayClose: 100,
@@ -2645,6 +2652,7 @@ OSD.GUI.updateFields = function() {
      // Update the OSD preview
      refreshOSDSwitchIndicators();
      updateCraftName();
+     updatePanServoPreview();
 };
 
 OSD.GUI.removeBottomLines = function(){
@@ -2663,8 +2671,6 @@ OSD.GUI.removeBottomLines = function(){
         }
     });
 };
-
-
 
 OSD.GUI.updateDjiMessageElements = function(on) {
     $('.display-field').each(function(index, element) {
@@ -3055,6 +3061,17 @@ TABS.osd.initialize = function (callback) {
             refreshOSDSwitchIndicators();
         });
 
+        // Functions for when pan servo settings change
+        $('#osdPanServoIndicatorShowDegrees').on('change', function() {
+            // Update the OSD preview
+            updatePanServoPreview();
+        });
+
+        $('#panServoOutput').on('change', function() {
+            // Update the OSD preview
+            updatePanServoPreview();
+        });
+
         // Function for when text for craft name changes
         $('#craft_name').on('keyup', function() {
             // Make sure that the craft name only contains A to Z, 0-9, spaces, and basic ASCII symbols
@@ -3226,6 +3243,46 @@ function refreshOSDSwitchIndicators() {
                     item.preview = FONT.symbol(SYM.SWITCH_INDICATOR_HIGH) + switchIndText;
                 }
             }
+        }
+    }
+
+    OSD.GUI.updatePreviews();
+}
+
+function updatePanServoPreview() {
+    // Show or hide the settings, based on of the feature is active.
+    if ($('#panServoOutput').val() === "0") {
+        $('#osd_pan_settings').hide();
+        $('#panServoOutput').parent().addClass('no-bottom');
+    } else {
+        $('#osd_pan_settings').show();
+        $('#panServoOutput').parent().removeClass('no-bottom');
+    }
+
+    // Update the panServoOutput select to be visibly easier to use
+    $('#panServoOutput option').each(function() {
+        if ($(this).val() === "0") {
+            $(this).text("OFF");
+        } else {
+            $(this).text("S" + $(this).val());
+        }
+    });
+
+    // Update the OSD preview based on settings
+    let generalGroup = OSD.constants.ALL_DISPLAY_GROUPS.filter(function(e) {
+        return e.name == "osdGroupGeneral";
+      })[0];
+
+    for (let si = 0; si < generalGroup.items.length; si++) {
+        if (generalGroup.items[si].name == "PAN_SERVO_CENTRED") {
+            let craftNameText = $('#craft_name').val();
+
+            if ($('#osdPanServoIndicatorShowDegrees').prop('checked')) {
+                generalGroup.items[si].preview = FONT.symbol(SYM.PAN_SERVO_IS_OFFSET_L) + '120' + FONT.symbol(SYM.DEGREES);
+            } else {
+                generalGroup.items[si].preview = FONT.symbol(SYM.PAN_SERVO_IS_OFFSET_L);
+            }
+            break;
         }
     }
 
